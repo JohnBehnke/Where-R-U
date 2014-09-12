22a4f71f5b571f09dfe80cd93b847506d9a17995
John Behnke <behnkej@me.com>
2014-09-12 13:39:18 -0400

    Rough UI Layout
    Added a new viewController for a 3rd tab, and did some arranging of
    tabs.

---
+++ iOS/Where R U?/Where R U?/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThridViewController.swift
+//  Where R U?
+//
+//  Created by John Behnke on 9/8/14.
+//  Copyright (c) 2014 John Behnke. All rights reserved.
+//
+
+import UIKit
+
+class ThridViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
