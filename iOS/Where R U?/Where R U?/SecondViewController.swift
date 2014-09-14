74a643408fd8db1bbef0c9b1f40db0e8a49defcc
John Behnke <behnkej@me.com>
2014-09-13 20:00:45 -0400

    GPS, UI, Layout
    Added some UI elements for use between platforms
    Added GPS data to the map so it starts at your current location (should
    be made way better) only for temp usage.

--- iOS/Where R U?/Where R U?/SecondViewController.swift
+++ iOS/Where R U?/Where R U?/SecondViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class SecondViewController: UIViewController {
-                            
+
+
+class SecondViewController: UIViewController , CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet var theMapView :MKMapView
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       if (CLLocationManager.locationServicesEnabled()){
+        
+        //the following code gets GPS data and places it on the map. Its just temp code to show it working. It should be put in its own .swift class or something. 
+            var locationManager = CLLocationManager()
+            locationManager = CLLocationManager()
+            locationManager.delegate = self;
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        
+            let location = locationManager.location
+            var latDelta:CLLocationDegrees = 0.01
+            var longDelta:CLLocationDegrees = 0.01
+            var Span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude , longitude: location.coordinate.longitude)
+            let region:MKCoordinateRegion = MKCoordinateRegionMake(center, Span)
+      
+            self.theMapView.setCenterCoordinate(center, animated: true)
+            self.theMapView.setRegion(region, animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +46,3 @@ class SecondViewController: UIViewController {
 
 
 }
-
