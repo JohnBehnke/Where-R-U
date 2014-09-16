6defc30f8bece22ec77ec53faf631f2afdb63517
minstz <minstz@rpi.edu>
2014-09-16 17:57:43 -0400

    Bug fix and cleanup
    Fixed bug involving new version Swift 1.0 and Xcode 6.1.
    Separated aspects of code into individual functions for fail and
    success.

--- iOS/Where R U?/Where R U?/SecondViewController.swift
+++ iOS/Where R U?/Where R U?/SecondViewController.swift
@@ -9,40 +9,49 @@
 import UIKit
 import MapKit
 
-
-
-class SecondViewController: UIViewController , CLLocationManagerDelegate, MKMapViewDelegate {
+class SecondViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate  {
     
-    @IBOutlet var theMapView :MKMapView
-   
+    var theMapView :MKMapView! = nil
+    var locationManager :CLLocationManager! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       if (CLLocationManager.locationServicesEnabled()){
         
-        //the following code gets GPS data and places it on the map. Its just temp code to show it working. It should be put in its own .swift class or something. 
-            var locationManager = CLLocationManager()
+        if (CLLocationManager.locationServicesEnabled()){
+
+            
+            //the following code gets GPS data and places it on the map. Its just temp code to show it working. It should be put in its own .swift class or something. <- Needs to happen
+            
             locationManager = CLLocationManager()
-            locationManager.delegate = self;
+            locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.requestAlwaysAuthorization()
             locationManager.startUpdatingLocation()
-        
-            let location = locationManager.location
-            var latDelta:CLLocationDegrees = 0.01
-            var longDelta:CLLocationDegrees = 0.01
-            var Span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
-            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude , longitude: location.coordinate.longitude)
-            let region:MKCoordinateRegion = MKCoordinateRegionMake(center, Span)
-      
-            self.theMapView.setCenterCoordinate(center, animated: true)
-            self.theMapView.setRegion(region, animated: true)
+            
+            //Will now begin tracking the user.  Will go to one of the 2 functions below (fail/success)
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //If at any time Location update does not work, display error message.  For debugging purposes only
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println("Error while updating location")
     }
-
-
+    
+    //If Location was successfully updated, this function is run
+    func locationManager(manager: CLLocationManager!, didUpdateLocations location:[AnyObject]) {
+        //println("Success")
+        
+        let location = locationManager.location
+        var latDelta:CLLocationDegrees = 0.01
+        var longDelta:CLLocationDegrees = 0.01
+        var Span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude , longitude: location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(center, Span)
+        
+        self.theMapView.setCenterCoordinate(center, animated: true)
+        self.theMapView.setRegion(region, animated: true)
+    }
+    
+    
 }
+
---
+++ iOS/Where R U?/Where R U?/cordinate_class.swift
@@ -0,0 +1,16 @@
+//
+//  cordinate_class.swift
+//  Where R U?
+//
+//  Created by Zachary Minster on 9/16/14.
+//  Copyright (c) 2014 John Behnke. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class test_class : CLLocationManagerDelegate, MKMapViewDelegate {
+    
+
+
+}
