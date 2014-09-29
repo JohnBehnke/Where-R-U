365b37c7c70e7d2507f516f142e8dccabbc0e24c
minstz <minstz@rpi.edu>
2014-09-29 14:56:13 -0400

    Work on some classes

--- iOS/Where R U?/Where R U?/Ride.swift
+++ iOS/Where R U?/Where R U?/Ride.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
+
 class Ride{
     
+//Variables - Driver (Person), list of participants(Person), list of person needed to be picked up, list of persons currently picked up, dates and times, locations(driver current, destinations - passenger current for pickup or static destination for dropoff)
     
     
-    
-    
-    
+    private var driver : Person
     
 }
