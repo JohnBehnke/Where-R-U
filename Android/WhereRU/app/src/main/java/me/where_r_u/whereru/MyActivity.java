c195bae18ce45f4cd32905e9a0814fe64ed45d88
Brian Kelley <bkelley620@gmail.com>
2014-11-21 10:58:56 -0500

    Material design

--- Android/WhereRU/app/src/main/java/me/where_r_u/whereru/MyActivity.java
+++ Android/WhereRU/app/src/main/java/me/where_r_u/whereru/MyActivity.java
@@ -128,6 +128,12 @@ public class MyActivity extends Activity implements ActionBar.TabListener {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
