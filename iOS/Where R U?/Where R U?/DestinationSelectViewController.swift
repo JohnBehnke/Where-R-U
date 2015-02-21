b1fcaacfcb0fd8238ea488f49ac85288c3bb4565
John Behnke <behnkej@me.com>
2015-02-21 17:03:58 -0500

    Added more ride creation options

---
+++ iOS/Where R U?/Where R U?/DestinationSelectViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DestinationSelectViewController.swift
+//  Where R U?
+//
+//  Created by John Behnke on 2/21/15.
+//  Copyright (c) 2015 John Behnke. All rights reserved.
+//
+
+import Foundation
