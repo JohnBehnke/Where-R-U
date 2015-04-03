7cdf0be89ce9f8b3a850801d5b6fbb97e54a6e09
minstz <minstz@rpi.edu>
2015-04-03 17:49:50 -0400

    Friend Requests
    Friend Requests now queries the parse database and displays the pending
    requests to the user. Bug with the table only updating after you switch
    back to view a second time.

--- iOS/Where R U?/Where R U?/FriendRequestViewController.swift
+++ iOS/Where R U?/Where R U?/FriendRequestViewController.swift
@@ -13,9 +13,63 @@ class FriendRequestViewController: UITableViewController,UITableViewDelegate, UI
 
     var settingsVC: SettingsViewController!
     
+    internal var friendRequests:[Request] = []
+    
     @IBOutlet var requestTable: UITableView!
 
-    var friendRequests:[String] = []
+    func Query() {
+        //query database for friend request to current user
+        var loadRequests:[Request] = []
+        var friendshipQuery = PFQuery(className: "Friends")
+        friendshipQuery.whereKey("user2", equalTo: PFUser.currentUser().objectId)
+        friendshipQuery.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            //because we dont like errors
+            if (error == nil) {
+                println("retrieved \(objects.count) requests")
+                
+                if let objects = objects as? [PFObject] {
+                    for object in objects {
+                        //get userId of sender
+                        var userID = object.objectForKey("user1") as String
+
+                        //query the user table for the name matching that userId
+                        var findUser:PFQuery = PFUser.query();
+                        findUser.whereKey("objectId",  equalTo: userID)
+                        //QUERYSEPTION
+                        findUser.getFirstObjectInBackgroundWithBlock {
+                            (user, error2: NSError!) -> Void in
+                            if (error2 == nil) {
+                                var username = user.objectForKey("username") as String
+        
+                                //SWIFT PLS STAHP Q.Q
+                                println(username)
+                                //usernmae is retrieved succesfully, but the new request is not being appended to array
+                                //Create a new request with the received name
+                                var newRequest:Request = Request(name: username)
+                                
+                                self.friendRequests.append(newRequest)
+                                println(self.friendRequests.count)
+                                //End Madness
+                                
+                            }
+                            else {
+                                println("Error getting name")
+                            }
+                        }
+                    }
+                }
+        
+            }
+            else {
+                println("Error getting requests")
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
     override func didReceiveMemoryWarning() {
         
@@ -25,6 +79,12 @@ class FriendRequestViewController: UITableViewController,UITableViewDelegate, UI
     
     override func viewWillAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        //test that works
+        var test:Request = Request(name: "WTFSWIFT")
+        friendRequests.append(test)
+        //real shit that doesnt
+        Query()
+        //data is not reloading im assuming its because the query takes longer than the table load
         requestTable.reloadData()
     }
     
@@ -33,11 +93,10 @@ class FriendRequestViewController: UITableViewController,UITableViewDelegate, UI
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        var thisRequest:Request! = friendRequests[indexPath.row]
         
-        
-        var cell: requestCell = tableView.dequeueReusableCellWithIdentifier("requestTableCell") as requestCell //this is the resualble cells
-            cell.requestSender.text = "Johnsucks"
-        
+        var cell: requestCell = tableView.dequeueReusableCellWithIdentifier("requestCell") as requestCell
+            cell.requestSender.text = thisRequest.getSender()
         return cell
     }
     
